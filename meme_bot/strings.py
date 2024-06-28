93db679114d604bfc1d43cb00e1a5256935485d4
RRV <79261047648@ya.ru>
2024-06-28 21:36:40 +0300

    Revert "New welcome message"
    This reverts commit 2212b3f9632ec0e5066d745127f5791d58deb5a3.

--- meme_bot/strings.py
+++ meme_bot/strings.py
@@ -3,6 +3,6 @@ from typing import Final
 HELP: Final[str] = (
     "Supported commands:\n/anec - random anecdote\n/meme - random meme\n/gmeme - gif meme\n/vmeme - video meme"
 )
-HELLO: Final[str] = "Hello, %USER%! Welcome to Meme bot. \n\n" + HELP
+HELLO: Final[str] = "Hello, %USER%!\n\n" + HELP
 CONTENT_ERROR: Final[str] = "Sorry, but there is a problem with content"
 UNKNOWN_COMMAND: Final[str] = "Unknown command. Type /help for help."
