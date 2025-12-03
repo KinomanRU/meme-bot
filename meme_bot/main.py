d265c05736e588e9e85b74ea651cafbf6a0dd20c
RRV <79261047648@ya.ru>
2025-12-03 14:30:16 +0300

    BOT_TOKEN moved from ini to env

--- meme_bot/main.py
+++ meme_bot/main.py
@@ -1,21 +1,30 @@
 import asyncio
 import logging
+from os import getenv
+import importlib.util
 from datetime import datetime
 from aiogram import Bot, Dispatcher
 import log_utils
 import proxy_utils
-from config import config
 from routers import router as main_router
+
+if importlib.util.find_spec("dotenv"):
+    from dotenv import load_dotenv
+
+    load_dotenv()
 
 log = logging.getLogger(name=__name__)
 
 
 async def main() -> None:
     log_utils.init_logging()
+    bot_token = getenv("BOT_TOKEN")
+    if not bot_token:
+        raise Exception("BOT_TOKEN environment variable is not set")
     dp = Dispatcher()
     dp.include_router(main_router)
     bot = Bot(
-        token=config.get("Bot", "Bot_Token"),
+        token=bot_token,
         session=proxy_utils.SESSION,
     )
     await dp.start_polling(bot)
